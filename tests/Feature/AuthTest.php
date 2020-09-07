fa51a354a2f3cd9fe3a87dea78dafb976f27a0dd
Karl Murray <karl@fwdmotion.studio>
2020-09-07 16:00:39 -0500

    Writing first tests

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use App\User;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_auth_register() {
+        $this->actingAs(factory(User::class)->create());
+        $reponse = $this->post('/api/register')->assertStatus(201);
+    }
+
+    public function test_auth_login() {
+        $this->actingAs(factory(User::class)->create());
+        $reponse = $this->post('/api/login')->assertOk();
+    }
+}
