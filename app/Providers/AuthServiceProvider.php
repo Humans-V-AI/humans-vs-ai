8214cf61d54842b7f8bdfc83c666f6f0f017188b
Karl Murray <karl@fwdmotion.studio>
2020-09-08 07:54:14 -0500

    Finished OAUTH

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,11 +22,8 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         $this->registerPolicies();
-        Passport::routes(function($router){
-            $router->forAccessTokens();
-        });
+        Passport::routes();
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
-window._ = require('lodash');
-window.Vue = require('vue');
-window.axios = require('axios');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+require('./bootstrap');
+
+window.Vue = require('vue');
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -11,18 +16,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('app', require('./App.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-import store from './store' 
+
 const app = new Vue({
-    store,
     el: '#app',
 });
